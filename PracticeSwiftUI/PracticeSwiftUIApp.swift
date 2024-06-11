903ce879428cc02159c951bac6f00e473eb9a793
0102JohnnyTd <rt2824.recruit.1022@gmail.com>
2024-06-11 10:00:13 +0900

    Initial Commit

---
+++ PracticeSwiftUI/PracticeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftUIApp.swift
+//  PracticeSwiftUI
+//
+//  Created by Johnny Toda on 2024/06/11.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
